b38c8486e48a336c2bf2726be176f7ee82798091
Macon Pegram <mpegram@captechventures.com>
2015-07-08 23:24:28 -0400

    almost done!

--- gitlab/client.js
+++ gitlab/client.js
@@ -4,7 +4,7 @@ var config = require('./config.js');
 exports.preHandlerProjectDetails = function (req, reply) {
     var projectId = req.payload.object_attributes.source_project_id;
     var action = req.payload.object_attributes.state;
-    if (action === "opened") {
+    if (action === "opened" || action == "reopened") {
         unirest.get(config.gitlabAPIBase + '/projects/' + projectId)
             .strictSSL(false)
             .header('Accept', 'application/json')
@@ -16,6 +16,7 @@ exports.preHandlerProjectDetails = function (req, reply) {
                 else {
                     var projectDetails = new Object();
                     projectDetails.id = response.body.id;
+                    projectDetails.name = response.body.path_with_namespace;
                     projectDetails.webUrl = response.body.web_url;
 
                     console.log('Project details retrieved: ' + JSON.stringify(projectDetails));
@@ -24,5 +25,7 @@ exports.preHandlerProjectDetails = function (req, reply) {
 
             });
     }
-    reply ();
+    else {
+        reply();
+    }
 }
--- gitlab/mergehandler.js
+++ gitlab/mergehandler.js
@@ -1,12 +1,35 @@
 var unirest = require("unirest");
+var config = require('./config.js');
 
 exports.handler = function(req, reply) {
     console.log("MR issued for repo: " + req.params.projRepo);
     console.log("Payload\n=========\n" + JSON.stringify(req.payload));
     var projectDetails = req.pre.projDetails;
-    if (projectDetails) {
-        var targetURL = projectDetails.webUrl + '/merge_requests/' + req.payload.object_attributes.iid;
+    var mergeDetails = req.payload.object_attributes;
+    if (projectDetails && mergeDetails) {
+        var targetURL = projectDetails.webUrl + '/merge_requests/' + mergeDetails.iid;
         console.log('View merge request at: ' + targetURL);
+        var webhookMessage = {
+            text : '*Merge Request:* ' + projectDetails.name
+            + ' *From:* ' + mergeDetails.source_branch
+            + ' *To:* ' + mergeDetails.target_branch
+            + '\n<' + targetURL +'>',
+            channel : config.slackChannel,
+            attachments : [
+                {
+                    image_url : 'http://blog.klocwork.com/wp-content/uploads/2009/11/sally-code-review-300x257.png'
+                }
+            ]
+        };
+        unirest.post(config.slackWebhookUrl)
+            .header('Accept', 'application/json')
+            .send("payload=" + JSON.stringify(webhookMessage))
+            .end(function (response) {
+                // Something went wrong.. log it.
+                if (response.statusCode > 200)
+                    console.log("[Slack Error Response]: " + response.body);
+            });
+
     }
     reply();
 };
