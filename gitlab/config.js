72496fb9bddeb6658dea6bc01da03fa17e332cc7
Macon Pegram <mpegram@captechventures.com>
2015-07-09 00:02:04 -0400

    stubbed config

--- gitlab/config.js
+++ gitlab/config.js
@@ -1,3 +1,7 @@
 exports.slackWebhookUrl = 'https://hooks.slack.com/services/YOUR/INCOMING/WEBHOOK';
-exports.gitlabPrivateToken = 'PRIVATE_TOKEN';
-exports.gitlabAPIBase = 'https://your.domain.com/api/v3';
+exports.gitlabPrivateToken = 'YOUR_PRIVATE_TOKEN';
+exports.gitlabAPIBase = 'https://your.server.com/api/v3';
+exports.slackChannel='#yourchannel';
+exports.memes=[
+    'http://your.apiserver.net:{your_api_port}/slack-gitlab/memes/codereview-got.jpg'
+]
