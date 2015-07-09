7c5c0742ff03ecaef3365ff3878c07ec0cffef53
Macon Pegram <mpegram@captechventures.com>
2015-07-09 00:15:27 -0400

    added a missing semi-colon

--- gitlab/config.js
+++ gitlab/config.js
@@ -4,4 +4,4 @@ exports.gitlabAPIBase = 'https://your.server.com/api/v3';
 exports.slackChannel='#yourchannel';
 exports.memes=[
     'http://your.apiserver.net:{your_api_port}/slack-gitlab/memes/codereview-got.jpg'
-]
+];
